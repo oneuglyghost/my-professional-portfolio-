535c94555e7a82d29ff43b794877ed8e505d0f09
Bernardo Alonso <bernardo4430@gmail.com>
2023-12-18 16:59:08 -0600

    fixed possitioning of work elements and added discription section to description-Box

--- assets:css/javascript.js
+++ assets:css/javascript.js
@@ -1,10 +1,15 @@
 let clickElementId;
-let selectedJobDescription;
+
 
 const Work = {
-    firstJob: "Horiseon Website was built using <br> HTML/CSS",
-    secondJob: "https://oneuglyghost.github.io/Horiseon-Social-Solution-Information-Website/",
-    thirdJob: "description",
+    firstJob: "Horiseon Website",
+    secondJob: "",
+    thirdJob: "",
+};
+const workDescription = {
+    firstJob: "snadksjnkdnaknskdn",
+    secondJob:"",
+    thirdJob:"",
 };
 
 // gets the id from element that i click on 
@@ -15,17 +20,19 @@ document.addEventListener('click', function (event) {
 
     for (const job in Work) { // loops through work properties
 
-        if (job == clickElementId){  // checks if any property matches the clicked id 
-            console.log("it works", Work[job]);
+        if (job == clickElementId){  // checks if any property in Work matches the clicked id 
+            // adds text to the element with that id
+            document.getElementById("work-Tittle").textContent = Work[job];
+        
+            if (workDescription[job]) {
+                document.getElementById("work-Description").textContent = workDescription[job];
+            } else {
+                console.log("no match");
+                null
+            }  
 
-            selectedJobDescription = Work[job];
-
-            document.getElementById("work-Description").textContent = selectedJobDescription;
-            break; // stops the loop if a match is made or when it gets to the end of list 
-        } else {
-            console.log("no match");
-            null
+            break; // stops the loop if a match is made or when it gets to the end of list
         }
     }
 });
-
+ 
